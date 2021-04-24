017bfff8f597d8661d052874f42c12e37c86faee
Zhang Xin <zhangxindev@gmail.com>
2021-04-24 19:35:30 +0800

    完成 header 组件

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,25 @@
 <template>
   <div id="app">
-    
+    <Header></Header>
   </div>
 </template>
+<script>
+import Header from './components/Header.vue'
 
-<style lang="scss">
+export default {
+  name: 'App',
+  components: {
+    Header
+  }
 
+}
+</script>
+<style lang="scss" scoped>
+  #html,
+  body {
+    * {
+      margin: 0;
+      padding: 0;
+    }
+  }
 </style>
